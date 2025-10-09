b059b750b5e12e7e410daca5086175ac746439b7
Mauro <maurodefalco96@gmail.com>
2025-10-10 00:31:16 +0200

    fix favicon calls

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,8 +8,16 @@ export default defineNuxtConfig({
     head: {
       title: 'Prismic + Nuxt Minimal Starter',
       htmlAttrs: { lang: 'en' },
-      meta: [{ charset: 'utf-8' }],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      meta: [{ charset: 'utf-8' },
+        { name: 'apple-mobile-web-app-title', content: 'Farbe'},
+      ],
+
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        {rel: 'apple-touch-icon', href: '/apple-touch-icon.png'},
+        {rel: 'icon', type: 'image/png', href: '/favicon-96x096.png', sizes: '96x96'},
+        {rel: 'icon', type: 'image/svg+xml', href: '/favicon-svg'},
+        {rel: 'manifest', href: '(site.manifest'}
+      ],
     }
   },
 
