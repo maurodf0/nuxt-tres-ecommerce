7dd1be052972da7c676b33ee4ab4d1ede8d9993c
Mauro <maurodefalco96@gmail.com>
2025-10-11 15:57:00 +0200

    Aggiungi il layout di default con integrazione Prismic e gestione SEO

---
+++ app/layouts/default.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+const prismic = usePrismic()
+const { data: settings } = await useAsyncData('settings',() =>
+  prismic.client.getSingle('settings', )
+)
+
+useSeoMeta({
+  title: settings.value?.data.site_title,
+  ogTitle: settings.value?.data.site_title,
+  description: settings.value?.data.meta_description,
+  ogDescription: settings.value?.data.meta_description,
+  ogImage: () => prismic.asImageSrc(settings.value?.data.meta_image)
+})
+</script>
+
+<template>
+  <div>
+    <pre>
+    {{ settings }}
+  </pre>
+    <slot />
+  </div>
+</template>
