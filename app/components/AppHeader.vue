d505974860ee50cf60b31dbc1ce5d7fc9521e38f
Mauro <maurodefalco96@gmail.com>
2025-10-12 19:02:42 +0200

    Aggiungi classi cta per migliorare lo stile dei link e del logo

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -7,22 +7,23 @@ defineProps<{
 </script>
 
 <template>
-  <header>
+  <header class="bg-[#DCD8D3]">
     <nav class="flex justify-between items-center">
-      <NuxtLink to="/">
-        <BrandSignature />
+      <NuxtLink to="/" class="cta py-mx ml-4">
+        <BrandSignature class="text-lg"/>
       </NuxtLink>
 
-      <ul>
+      <ul class="xl:w-[calc(40%+2rem)] flex items-center pl-8 pr-4 backdrop-blur gap-5">
        <li v-for="link in settings?.data.navigation" :key="link.key">
-          <PrismicLink :field="link" />
+          <PrismicLink :field="link" class="cta"/>
         </li>
-      </ul>
-      <li>
-        <NuxtLink to="/cart">
-          Cart
+          <li class="ml-auto">
+        <NuxtLink to="/cart" class="cta">
+          Cart (0)
         </NuxtLink>
       </li>
+      </ul>
+
 
     </nav>
   </header>
