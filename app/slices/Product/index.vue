8e297a9436b37504d2db7f2d2c691d6d89515a21
Mauro <maurodefalco96@gmail.com>
2025-10-25 19:03:53 +0200

    templating product WIP

--- app/slices/Product/index.vue
+++ app/slices/Product/index.vue
@@ -21,8 +21,30 @@ const product = computed( () => {
 
 <template>
 
-   <Slidein v-if="product">
-		{{product }}
+   <Slidein 
+	 v-if="product"
+	 as="article"
+	 class="bounded rich-text min-h-[150vh] flex flex-col justify-center">
+			<header :id="product.uid" class="rich-text pt-[25vh]">
+				<PrismicRichText :field="product.data?.name" />
+					<p aria-label="Price">TODO / ROLL</p>
+			</header>
+			<section class="rich-text">
+				<h3 class="sr-only">Description</h3>
+				<PrismicRichText :field="product.data?.description" />
+			</section>
+			<section class="rich-text">
+				<h3 class="sr-only">Charateristics</h3>
+				<PrismicTable 
+					:field="product.data?.characteristic"
+					:components="{
+						table: xxx,
+						tbody: yyy,
+						tr: Div,
+						th: dt,
+						td: dd
+					}" />
+			</section>
 	 </Slidein>
 
    <Slidein v-else>
