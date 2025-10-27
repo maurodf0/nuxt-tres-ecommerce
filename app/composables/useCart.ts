360757c2ba1e3233929f77abba5043f3ff28d0ae
Mauro <maurodefalco96@gmail.com>
2025-10-28 00:16:56 +0100

    Aggiunta delle funzioni upsertItem, removeItem e clear al tipo useCartReturnType

--- app/composables/useCart.ts
+++ app/composables/useCart.ts
@@ -12,7 +12,10 @@ type CartItems = Record<string, CartItem>
 type useCartReturnType = {
   items: unknown,
   totalPrice: ComputedRef<number>,
-  totalItems: ComputedRef<number>
+  totalItems: ComputedRef<number>,
+  upsertItem: (item: CartItem) => void,
+  removeItem: (item: CartItem) => void,
+  clear: () => void,
 }
 
 export const useCart = () => {
