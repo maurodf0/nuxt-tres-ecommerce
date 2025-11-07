74b4a254130cdf6fc667edc6022540f656b9f646
Mauro <maurodefalco96@gmail.com>
2025-11-08 00:36:55 +0100

    Aggiorna l'intensità della luce direzionale e modifica le impostazioni delle ombre in TScene

--- app/components/TScene.vue
+++ app/components/TScene.vue
@@ -20,24 +20,25 @@
           />
       </TresMesh>
 
-      <TresDirectionalLight 
-        cast-shadow
-        :position="[-8,6,20]"
-        :intensity="0.5"
-        :shadow-mapSize-width="512"
-        :shadow-mapSize-height="512"
-        :shadow-camera-left="-16"
-        :shadow-camera-right="16"
-        :shadow-camera-top="16"
-        :shadow-camera-bottom="-16"
-        :color="0xFFFFFF"
-        />
-        <SoftShadows :size="40" :samples="10" />
+<TresDirectionalLight
+		cast-shadow
+		:position="[-8, 6, 20]"
+		:intensity=".5"
+		:shadow-mapSize-width="512"
+		:shadow-mapSize-height="512"
+		:shadow-camera-left="-16"
+		:shadow-camera-right="16"
+		:shadow-camera-top="16"
+		:shadow-camera-bottom="-16"
+		:color="0xFFFFFF"
+	/>
+
+	<SoftShadows :size="50" :samples="10" />
 
 	<Suspense>
 		<Environment
 			files="/textures/lobby.hdr"
-			:environment-intensity="1"
+			:environment-intensity="0.25"
 		/>
 	</Suspense>
 </template>
