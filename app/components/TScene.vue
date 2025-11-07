2e359ba038db2758511bb85e713023a3e7209b98
Mauro <maurodefalco96@gmail.com>
2025-11-08 00:22:10 +0100

    TScene

--- app/components/TScene.vue
+++ app/components/TScene.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-
+  /* eslint-disable vue/attribute-hyphenation */
 </script>
 
 <template>
@@ -21,7 +21,22 @@
 
       <TresDirectionalLight 
         cast-shadow
-        :position="[-6,6,20]"
-        :intensity=".5"
+        :position="[-8,6,20]"
+        :intensity="0.5"
+        :shadow-mapSize-width="512"
+        :shadow-mapSize-height="512"
+        :shadow-camera-left="-16"
+        :shadow-camera-right="16"
+        :shadow-camera-top="16"
+        :shadow-camera-bottom="-16"
+        :color="0xFFFFFF"
         />
+        <SoftShadows :size="40" :samples="10" />
+
+	<Suspense>
+		<Environment
+			files="/textures/lobby.hdr"
+			:environment-intensity="0.25"
+		/>
+	</Suspense>
 </template>
