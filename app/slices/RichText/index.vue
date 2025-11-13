516253da39632f59fe5695e69f9dd87cf89afebd
Mauro <maurodefalco96@gmail.com>
2025-11-13 18:44:17 +0100

    fix(RichText): correct syntax error in Slidein component props
    Ensure proper comma separation and handle undefined product UID case in the Slidein component attributes.

--- app/slices/RichText/index.vue
+++ app/slices/RichText/index.vue
@@ -11,8 +11,9 @@ defineProps(getSliceComponentProps<Content.RichTextSlice>(
 	
   <Slidein 
 	v-bind="getSceneAttributes({
-		position: 'center'
-		model:})"
+		position: 'center',
+		model: $prismic.isFilled.contentRelationship(slice.primary.product)? slice.primary.product.uid : undefined
+	})"
 	class="bounded rich-text flex flex-col justify-center"
 	:class="{
 		'min-h-[40vh]' : slice.variation !== 'fullscreen',
